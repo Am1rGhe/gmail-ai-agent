65142736d980b877e2295d56dce8908a9d7debc7
Amir <amiramir2582004@.com>
2026-03-08 15:10:36 -0400

    add singInButton component

---
+++ app/components/SingInButton.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { signIn } from "next-auth/react";
+
+export function SignInButton() {
+  return (
+    <button
+      type="button"
+      onClick={() => signIn("google", { callbackUrl: "/inbox" })}
+      className="rounded-full bg-foreground px-8 py-3 text-base font-medium text-background transition-colors hover:opacity-90 "
+      
+    >
+      Start With Google
+    </button>
+  );
+}
